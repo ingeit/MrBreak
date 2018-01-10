700e1d166905a8208e88879bd8819fe16a2342f2
Kevin Shionen Gomez Veliz <masterk@MacBook-Pro-de-Kevin.local>
2018-01-10 14:01:59 -0300

    styling MrBrak

--- front/src/pages/home/home.ts
+++ front/src/pages/home/home.ts
@@ -6,9 +6,17 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public productos:any= [];
 
+  
   constructor(public navCtrl: NavController) {
-
+    for(let i=0;i<36;i++){ 
+      let prodTemp = {id:i,name: 'T. Jamon/Salame + LicuadoAPruebaDeAgua'};
+      if(prodTemp.name.length > 26){
+        prodTemp.name = prodTemp.name.slice(0,25) + '...';
+      }
+      this.productos.push(prodTemp);
+    }
   }
 
 }
