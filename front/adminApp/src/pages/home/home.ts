d788224e1372fc5be3a09486fe53ab8ededfbde4
Ricky Bruno <ricardobruno_89@hotmail.com>
2018-01-16 19:53:58 -0300

    subiendo imagenes

--- front/adminApp/src/pages/home/home.ts
+++ front/adminApp/src/pages/home/home.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+ 
   constructor(public navCtrl: NavController) {
+      
+  }
 
+  imagen(evento){
+    console.log(evento);
+    let reader = new FileReader();
+    let file = evento.target.files[0];
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      let base64 = reader.result.split(',')[1]
+      console.log(base64);
+    }    
   }
 
 }
